9aa42e50e67d774c699c6777eccb0aab754d3e52
youssef0096 <joe_postiga4ever@hotmail.com>
2018-09-01 06:39:19 -0700

    todo Get router

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send(todos);
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
+});
+
 
 
 app.listen(3000, () => {
